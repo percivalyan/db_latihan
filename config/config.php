cb61fc4d680855ef731b7040e04afc0666ae7c89
percivalyan <yoryan8888@gmail.com>
2024-10-25 17:20:05 +0700

    main: Updated

--- config/config.php
+++ config/config.php
@@ -1,19 +1,13 @@
 <?php
 $host = 'localhost';
-$db   = 'universitas';
-$user = 'root'; // Ganti dengan username database Anda
-$pass = ''; // Ganti dengan password database Anda
+$db   = 'db_latihan';
+$user = 'root';
+$pass = '';
 
-// Membuat koneksi
 $connection = mysqli_connect($host, $user, $pass, $db);
 
-// Memeriksa koneksi
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-// Kode Anda selanjutnya dapat ditulis di sini
-
-// Menutup koneksi
 // mysqli_close($connection);
 ?>
